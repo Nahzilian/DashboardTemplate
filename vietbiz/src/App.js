62382f3771bde0d27a7b6e71443a3107e7b5693e
Nahzilian <nahz3112@gmail.com>
2021-03-14 23:01:27 -0400

    Homepage init

--- vietbiz/src/App.js
+++ vietbiz/src/App.js
@@ -1,8 +1,10 @@
 import './components/assets/stylesheets/App.css';
+import Homepage from './components/Homepage/Homepage'
+
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
